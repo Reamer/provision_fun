cabd2fc35d0ac4fba9dc8630fef82dc4a3e70a33
Philipp Dallig <philipp.dallig@gmail.com>
2017-10-14 13:40:56 +0200

    Add some more test with count

--- coreos/coreos_fun.tf
+++ coreos/coreos_fun.tf
@@ -1,3 +1,30 @@
+variable "slave-amount" {
+  type = "string"
+}
+
+// blank 10GB image for net install.
+resource "libvirt_volume" "coreos-qcow2" {
+  name = "coreos-qcow2-${count.index}"
+  pool = "default"
+  format = "qcow2"
+  size = 10000000000
+  count = "${var.slave-amount}"
+}
+
 resource "libvirt_domain" "terraform_test" {
-  name = "terraform_test"
+  name = "terraform_test-${count.index}"
+  vcpu = 2
+  memory = 1024
+
+  network_interface {
+       bridge = "virbr1"
+  }
+  graphics {
+    type = "vnc"
+    listen_type = "address"
+  }
+  disk {
+    volume_id = "${element(libvirt_volume.coreos-qcow2.*.id, count.index)}"
+  }
+  count = "${var.slave-amount}"
 }
